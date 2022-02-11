afd85c1df9b4c99ce975f1baa15c6ce7e8d48ea8
Shiva <shanshiva@outlook.com>
2022-02-11 17:00:49 +0530

    Added Function to exchange ETH & ERC20 token

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -28,10 +28,29 @@ contract DEX {
   }
 
   function price(uint256 input_amount, uint256 input_reserve, uint256 output_reserve) public pure returns (uint256) {
+    
     uint256 input_amount_with_fee = input_amount * (997);
     uint256 numerator = input_amount_with_fee * (output_reserve);
     uint256 denominator = (input_reserve * (1000)) + (input_amount_with_fee);
     return numerator / denominator;
+
+  }
+
+  function ethToToken() public payable returns (uint256) {
+
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 tokens_bought = price(msg.value, address(this).balance - (msg.value), token_reserve);
+    require(token.transfer(msg.sender, tokens_bought));
+    return tokens_bought;
+
+  }
+
+  function tokenToEth(uint256 tokens) public returns (uint256) {
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 eth_bought = price(tokens, token_reserve, address(this).balance);
+    payable(msg.sender).transfer(eth_bought);
+    require(token.transferFrom(msg.sender, address(this), tokens));
+    return eth_bought;
   }
 
   
